3b03e766cf07f7beb82f4515e0400f3fd920ec8e
Amit Kumar <amitkumar212501@example.com>
2026-09-04 10:45:56 +0530

    hero LWC component of MyPortfolio 1.0

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import Image_url from '@salesforce/resourceUrl/experiencecloudimage';
+
+export default class Hero extends LightningElement {
+
+    heroImage=Image_url + '/Experience_Cloud_Project/hero1.jpg';
+}
