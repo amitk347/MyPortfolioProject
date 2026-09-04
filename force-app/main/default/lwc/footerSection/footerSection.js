9786d72201919c7417e2d8e4f2db3ada865399f9
Amit Kumar <amitkumar212501@example.com>
2026-09-04 15:04:01 +0530

    footerSection LWC Component of MyPortfolio 1.0

---
+++ force-app/main/default/lwc/footerSection/footerSection.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import Image_url from '@salesforce/resourceUrl/experiencecloudimage';
+export default class FooterSection extends LightningElement {
+
+    youtubeLogo = Image_url + '/Experience_Cloud_Project/Myyoutube1.jpg';
+    instagramLogo = Image_url + '/Experience_Cloud_Project/Myinstagram1.jpg';
+    linkedInLogo = Image_url + '/Experience_Cloud_Project/MyLinkedIn1.jpg';
+    amitLogo = Image_url + '/Experience_Cloud_Project/AmitLogo1.jpg';
+}
